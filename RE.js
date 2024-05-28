4d1f29a10983ffcdf7f11b4f575c376a3edda26c
Paresh <pareshdhande10@gmail.com>
2024-05-28 09:21:42 +0530

    Initial commit

---
+++ RE.js
@@ -0,0 +1,90 @@
+function a(){
+    // copy and paste src from html file ( this is for changing img after clicking on name of that bike)
+    //create fun here & call them in ul (using onclick event)
+    //give id to 1st img only and then use same id for all images(coz al changes are done at same place)
+    document.getElementById("abc").src="./images/hunter-350-motorcycle-home.avif";
+}
+
+function b(){
+    document.getElementById("abc").src="./images/scram-411.avif";
+}
+
+function c(){
+    document.getElementById("abc").src="./images/classic-350-motorcycle-section.avif";
+}
+
+function d(){
+    document.getElementById("abc").src="./images/interceptor-homepage.avif";
+}
+
+function e(){
+    document.getElementById("abc").src="./images/continental-gt-homepage.avif";
+}
+
+function f(){
+    document.getElementById("abc").src="./images/royal-enfield-meteor-new-banner.avif";
+}
+
+function g(){
+    document.getElementById("abc").src="./images/sleethblack-banner-des.avif";
+}
+
+function h(){
+    document.getElementById("abc").src="./images/banner.avif";
+}
+
+function i(){
+    document.getElementById("abc").src="./images/super-meteor-650.avif";
+}
+
+function j(){
+    document.getElementById("def").src="./images/rider-mania.avif";
+}
+
+function k(){
+    document.getElementById("def").src="./images/Mapped.avif";
+}
+
+function l(){
+    document.getElementById("ghi").src="./images/11.avif";
+}
+
+function m(){
+    document.getElementById("ghi").src="./images/12.avif";
+}
+
+function n(){
+    document.getElementById("ghi").src="./images/13.avif";
+}
+
+function o(){
+    document.getElementById("ghi").src="./images/14.avif";
+}
+
+function p(){
+    document.getElementById("ghi").src="./images/15.avif";
+}
+
+function q(){
+    document.getElementById("ghi").src="./images/16.avif";
+}
+
+function r(){
+    document.getElementById("jkl").src="./images/21.avif";
+}
+
+function s(){
+    document.getElementById("jkl").src="./images/22.avif";
+}
+
+function t(){
+    document.getElementById("mno").src="./images/31.avif";
+}
+
+function u(){
+    document.getElementById("mno").src="./images/32.avif";
+}
+
+function v(){
+    document.getElementById("mno").src="./images/33.avif";
+}
